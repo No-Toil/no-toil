cb09857016cc786568775f7608d35866c4fd14cb
E Harrison <har16020@byui.edu>
2024-01-12 09:09:33 -0800

    closes #31

--- updateserver.py
+++ updateserver.py
@@ -1,4 +1,4 @@
-import os, sys, manage
+import os, sys, manage, random, string
 
 def main():
    # "cd ../No-Toil-Task-Tracker"
@@ -10,18 +10,27 @@ def main():
       # pull all code changes
       print(">> GIT")
       os.system(f"git pull")
-   except:
+   except Exception:
       print(">> GIT ERROR")
 
    try:
       # secret file
-      secret_file_path = "djangoproject/private/secret_key.txt"
+      private_folder_path = "djangoproject/private"
+      try:
+         os.makedirs(private_folder_path)
+      except FileExistsError as e:
+         # directory already exists
+         pass
+      secret_file_path = f"{private_folder_path}/secret_key.txt"
       secret_file_found = os.path.isfile(secret_file_path)
-      if secret_file_found: print(secret_file_found)
-      else: raise Exception(f"secret_file_found: {secret_file_found}")
-   except e:
-      print("error handling secret file.")
-      print(e)
+      with open(secret_file_path, 'r' if secret_file_found else 'w') as f:
+         if not secret_file_found:
+            print("generating secret key")
+            secret_key_text = f"secret key {random_text()}"
+            f.write(secret_key_text)
+   except Exception as e:
+      print("error handling secret file.", e)
+      raise e
 
    try:
       # update python packages
@@ -29,7 +38,7 @@ def main():
       print("   updating packages...")
       os.system(f"pip -q install --upgrade pip")
       os.system(f"pip -q install -r requirements.txt --no-warn-script-location")
-   except:
+   except Exception:
       print(">> PIP ERROR")
 
    try:
@@ -48,17 +57,23 @@ def main():
             os.system(f"npm install npm@latest")
             os.system(f"npm install --legacy-peer-deps")
             os.system(f"npm outdated")
-         except:
+         except Exception:
             print(">> NPM ERROR")
    except: pass
 
    print("\nUpdate successful, press enter to exit.")
    if "win" in sys.platform: input("--") # keep window open
 
+def random_text(length = 10_000):
+   text = ""
+   for i in range(length):
+      text += random.choice(string.ascii_letters)
+   return text
+
 if __name__ == "__main__":
    try:
       main()
-   except e:
+   except Exception as e:
       # once error logging is set up, replace below with logic to forward files to repository/developer email
       current_developer = """
          Elijah Harrison
